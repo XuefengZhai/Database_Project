9f772f9e53d25f1b5ec415454882ae3a451d1785
XuefengZhai <xzhai@cmu.edu>
2013-11-26 21:05:25 -0500

    Edited View2

--- Views/View2.sql
+++ Views/View2.sql
@@ -4,7 +4,7 @@
 
 --set the format!!
 --Assume that if a lot is empty then there is no house_id assigned to the lot.
-CREATE OR REPLACEVIEW view_enptylot AS
+CREATE OR REPLACE VIEW view_enptylot AS
 	SELECT l.lot_id, l.latitude, l.longtitude, l.subdivision_id, s.name
 	FROM Lot l
 	LEFT JOIN Subdivition s on l.subdivision_id = s.subdivision_id
