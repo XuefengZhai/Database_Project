98293a06f8999572a7a2c252bbe45544a96ef42f
XuefengZhai <xzhai@cmu.edu>
2013-12-03 02:06:34 -0500

    Update View 1

--- Views/View1.sql
+++ Views/View1.sql
@@ -1,16 +1,18 @@
 --Views
 
---1. Status of all unfinishing homes
+--1. Customer information of all the unfinished houses
 
 
---set the format!!
+--set the format??
 --Assume that if the construction is not finished, the end_date is NULL.
 CREATE OR REPLACE VIEW view_unfinishing AS
-	SELECT h.house_id, h.lot_id, con.contract_id, con.customer_id, 
-				 c.first_name AS FN, c.last_name AS LN, c.phone_number AS PhoneNo
-	FROM House h
-	LEFT JOIN Construction_project cp on h.construction_project_id = cp.construction_project_id
-	LEFT JOIN Contract con on h.contract_id = con.contraci_id
-	LEFT JOIN Customer c on con.customer_id = c.customer_id
+	SELECT h.house_id, l.lot_id, con.contract_id, c.customer_id, 
+				 c.first_name AS FN, c.last_name AS LN, c.phone_number AS PhoneNo,
+				 cp.estimated_end_date AS Estimated_End_Date
+	FROM construction_project cp
+	LEFT JOIN house h on h.house_id = cp.house_id
+	LEFT JOIN contract con on cp.house_id = con.house_id
+	LEFT JOIN customer_contract cc on con.contract_id = cc.contract_id
+	LEFT JOIN Customer c on cc.customer_id = c.customer_id
 	WHERE cp.end_date = NULL;
 				 
