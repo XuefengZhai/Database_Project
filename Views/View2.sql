f71ebd9d2468e5dd0718a8ae2dce104ad35a76db
XuefengZhai <xzhai@cmu.edu>
2013-12-03 02:08:36 -0500

    Update View2

--- Views/View2.sql
+++ Views/View2.sql
@@ -2,7 +2,7 @@
 
 --2. All empty lot
 
---set the format!!
+--set the format?!
 --Assume that if a lot is empty then there is no house_id assigned to the lot.
 CREATE OR REPLACE VIEW view_enptylot AS
 	SELECT l.lot_id, l.latitude, l.longtitude, l.subdivision_id, s.name
