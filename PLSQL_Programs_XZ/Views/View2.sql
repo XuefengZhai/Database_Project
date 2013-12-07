18ab45e58b6aea6c6893ddf65930771231a92920
XuefengZhai <xzhai@cmu.edu>
2013-12-07 16:15:03 -0500

    Updated View2

--- PLSQL_Programs_XZ/Views/View2.sql
+++ PLSQL_Programs_XZ/Views/View2.sql
@@ -4,8 +4,8 @@
 
 --set the format?!
 --Assume that if a lot is empty then there is no house_id assigned to the lot.
-CREATE OR REPLACE VIEW view_enptylot AS
-	SELECT l.lot_id, l.latitude, l.longtitude, l.subdivision_id, s.name
+CREATE OR REPLACE VIEW view_emptylot AS
+	SELECT l.lot_id, l.latitude, l.longitude, l.subdivision_id, s.name
 	FROM Lot l
-	LEFT JOIN Subdivition s on l.subdivision_id = s.subdivision_id
-	WHERE house_id = NULL;
+	LEFT JOIN Subdivision s on l.subdivision_id = s.subdivision_id
+	WHERE house_id is NULL;
