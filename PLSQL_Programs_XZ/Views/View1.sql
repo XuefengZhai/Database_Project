03e73bf77cac0357fb42ab8a0a9044dd2f385e1f
XuefengZhai <xzhai@cmu.edu>
2013-12-07 16:08:46 -0500

    Update View1

--- PLSQL_Programs_XZ/Views/View1.sql
+++ PLSQL_Programs_XZ/Views/View1.sql
@@ -6,7 +6,7 @@
 --set the format??
 --Assume that if the construction is not finished, the end_date is NULL.
 CREATE OR REPLACE VIEW view_unfinishing AS
-	SELECT h.house_id, l.lot_id, con.contract_id, c.customer_id, 
+	SELECT h.house_id, con.contract_id, c.customer_id, 
 				 c.first_name AS FN, c.last_name AS LN, c.phone_number AS PhoneNo,
 				 cp.estimated_end_date AS Estimated_End_Date
 	FROM construction_project cp
@@ -14,5 +14,5 @@ CREATE OR REPLACE VIEW view_unfinishing AS
 	LEFT JOIN contract con on cp.house_id = con.house_id
 	LEFT JOIN customer_contract cc on con.contract_id = cc.contract_id
 	LEFT JOIN Customer c on cc.customer_id = c.customer_id
-	WHERE cp.end_date = NULL;
-				 
+	WHERE cp.end_date is NULL
+	AND con.is_terminated like 'N';
