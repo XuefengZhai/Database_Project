c76d973fef2c2035dfc5899827c26e4634da1581
Benjamin Gummoe <b.gummoe@gmail.com>
2013-12-08 20:56:53 -0500

    ADDED INDEXES TO MASTER

--- master.sql
+++ master.sql
@@ -1792,3 +1792,14 @@ EXCEPTION
 		DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM || '.');
 END;
 /
+
+--CREATE INDEXES
+DROP INDEX ind_contract_cust_id;
+DROP INDEX ind_contract_cntrct_id;
+DROP INDEX ind_prj_house_id;
+DROP INDEX ind_prj_pm_id;
+
+CREATE INDEX ind_contract_cust_id on CUSTOMER_CONTRACT (customer_id); 
+CREATE INDEX ind_contract_cntrct_id on CUSTOMER_CONTRACT (contract_id); 
+CREATE INDEX ind_prj_house_id on CONSTRUCTION_PROJECT (house_id); 
+CREATE INDEX ind_prj_pm_id on CONSTRUCTION_PROJECT (project_manager_employee_id); 
