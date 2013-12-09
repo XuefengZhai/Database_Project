652ee8b3d844fc662d741081c96011b5a9101221
XuefengZhai <xzhai@cmu.edu>
2013-12-09 13:50:43 -0500

    Remove reports from the master.sql and put them in seperate report files

---
+++ G3_eggshell/G3_Report1.sql
@@ -0,0 +1,50 @@
+--Report1
+
+--1.Construction status of a given house
+
+CREATE OR REPLACE PROCEDURE display_home_status
+	( house_no IN house.house_id%TYPE
+	)
+AS
+
+FN Customer.first_name%TYPE;
+LN Customer.last_name%TYPE;
+HN Contract.house_id%TYPE;
+SD Construction_project.start_date%TYPE;
+ED Construction_project.end_date%TYPE;
+EsD Construction_project.estimated_end_date%TYPE;
+
+BEGIN
+	SELECT Customer.first_name, Customer.last_name, Contract.house_id, 
+				 Construction_project.start_date, Construction_project.end_date,
+				 Construction_project.estimated_end_date
+  	INTO FN, LN, HN, SD, ED, EsD
+	FROM Customer LEFT JOIN
+	Customer_contract on Customer.customer_id = Customer_contract.customer_id LEFT JOIN
+	Contract on Customer_contract.contract_id = Contract.contract_id LEFT JOIN
+	House on Contract.house_id = House.house_id LEFT JOIN
+	Construction_project on House.house_id = Construction_project.house_id
+	WHERE House.house_id = house_no
+	and contract.is_terminated = 'N';
+	
+	IF ED is NULL THEN
+		 DBMS_OUTPUT.PUT_LINE('The construction project of customer: '||FN||' '||LN|| 
+		 											  ' for house No.'||HN||' has not finished yet. '
+		 											  ||'It started on '||SD||
+		 										    '. The estimated finish date is: '||EsD||'.');
+	ELSE
+		DBMS_OUTPUT.PUT_LINE('The construction project of customer: '||FN||' '||LN|| 
+		 											  ' for house No.'||HN||' has finished! '
+		 											  ||'It started on '||SD|| 
+		 											  '. It finished on '||ED||'.');
+	
+	END IF;
+EXCEPTION
+	WHEN NO_DATA_FOUND THEN
+			DBMS_OUTPUT.PUT_LINE ('No such house.');
+	WHEN OTHERS THEN
+		 DBMS_OUTPUT.PUT_LINE ('The PLSQL procedure executed by '||USER||
+		 	' returned and unhandled exception on '||SYSDATE||'.');
+
+END;
+/
---
+++ G3_eggshell/G3_Report2.sql
@@ -0,0 +1,48 @@
+
+--Report2
+--2.Which project manager works on which construction project. (Assignment)
+
+
+
+CREATE OR REPLACE PROCEDURE PM_Assignment
+AS
+CP construction_project%ROWTYPE;
+FN employee.first_name%TYPE;
+LN employee.last_name%TYPE;
+
+CURSOR c1 is
+	SELECT * FROM CONSTRUCTION_PROJECT;
+
+BEGIN
+	OPEN c1;
+	LOOP
+		FETCH c1 INTO CP;
+		EXIT WHEN c1%NOTFOUND;
+		
+		SELECT e.first_name, e.last_name
+		INTO FN,LN
+		FROM employee e
+		WHERE e.employee_id = CP.project_manager_employee_id;
+		
+		DBMS_OUTPUT.PUT_LINE('The employee No.'||CP.project_manager_employee_id||
+										' Name:'||FN||' '||LN||
+										' is working on project No.'||CP.construction_project_id||
+										'.');
+		DBMS_OUTPUT.NEW_LINE;
+	END LOOP;
+	Close c1;
+
+EXCEPTION
+	WHEN NO_DATA_FOUND THEN
+			DBMS_OUTPUT.PUT_LINE ('No such employee.');
+	WHEN INVALID_CURSOR THEN
+		DBMS_OUTPUT.PUT_LINE ('The cursor is invalid.');
+	WHEN CURSOR_ALREADY_OPEN THEN
+		DBMS_OUTPUT.PUT_LINE ('The cursor is already opened.');
+	WHEN OTHERS THEN
+		 DBMS_OUTPUT.PUT_LINE ('The PLSQL procedure executed by '||USER||
+		 	' returned and unhandled exception on '||SYSDATE||'.');
+
+
+END;
+/
